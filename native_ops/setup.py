fb74a74473ce0deb2cea527697503a438f62ab7b
Pritika Vipin <65793273+Pritiks23@users.noreply.github.com>
2026-07-25 11:53:07 -0700

    Update setup.py

--- native_ops/setup.py
+++ native_ops/setup.py
@@ -1,1 +1,56 @@
-"""Build script for native operations extensions."""
+from setuptools import setup
+from torch.utils.cpp_extension import (
+    BuildExtension,
+    CUDAExtension
+)
+
+
+setup(
+
+    name="native_ops",
+
+    ext_modules=[
+
+        CUDAExtension(
+
+            name="native_ops",
+
+            sources=[
+
+                "native_ops.cpp",
+
+                "normalize_kernel.cu"
+
+            ],
+
+            extra_compile_args={
+
+                "cxx": [
+
+                    "-O3"
+
+                ],
+
+                "nvcc": [
+
+                    "-O3",
+
+                    "--use_fast_math"
+
+                ]
+
+            }
+
+        )
+
+    ],
+
+
+    cmdclass={
+
+        "BuildExtension":
+            BuildExtension
+
+    }
+
+)
