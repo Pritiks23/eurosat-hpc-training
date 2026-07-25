cb63fbb148f589f30acf86288072bb8eaafb1ebf
Pritika Vipin <65793273+Pritiks23@users.noreply.github.com>
2026-07-25 11:54:04 -0700

    Update native_ops.cpp

--- native_ops/native_ops.cpp
+++ native_ops/native_ops.cpp
@@ -1,1 +1,89 @@
-// Native C++ extension source placeholder.
+// Native C++ extension source placeholder. This is the bridge between Python and CUDA.Python cannot directly call: __global__ void kernel() So we create bindings.
+#include <torch/extension.h>
+
+
+// CUDA function declaration
+
+torch::Tensor normalize_cuda(
+    torch::Tensor input,
+    float mean,
+    float std
+);
+
+
+
+torch::Tensor normalize_cpu(
+    torch::Tensor input,
+    float mean,
+    float std
+);
+
+
+
+
+torch::Tensor normalize_images(
+
+    torch::Tensor input,
+
+    float mean,
+
+    float std,
+
+    bool use_cuda
+
+)
+
+{
+
+
+    if(use_cuda)
+
+    {
+
+        return normalize_cuda(
+            input,
+            mean,
+            std
+        );
+
+    }
+
+
+    else
+
+    {
+
+        return normalize_cpu(
+            input,
+            mean,
+            std
+        );
+
+    }
+
+}
+
+
+
+
+PYBIND11_MODULE(
+
+    TORCH_EXTENSION_NAME,
+
+    m
+
+)
+
+{
+
+    m.def(
+
+        "normalize_images",
+
+        &normalize_images,
+
+        "Image normalization CPU/CUDA"
+
+    );
+
+}
